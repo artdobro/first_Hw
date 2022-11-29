7e41bea7b1dbb94d70019122bd33c9f71d01e9b9
artdobro <105646581+artdobro@users.noreply.github.com>
2022-11-29 22:35:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // first part
-// a
-const a = "Привіт"
-console.log(typeof a)
+// 1
+const aa = "Привіт"
+console.log(typeof aa)
 
-const b = 123
-console.log(typeof b)
+const bb = 123
+console.log(typeof bb)
 
-const c = true
-console.log(typeof c)
+const cc = true
+console.log(typeof cc)
 
-const d = "true"
-console.log(typeof d)
-// b
+const dd = "true"
+console.log(typeof dd)
+// 2
 const num = 9
 const txt = "Hello world maybe"
 const bool = true
 const none = null
-// not Sophie
+// not Sophie)
 const name = undefined
 
 let numby = 10
@@ -33,3 +33,53 @@ const myAge = 14
 const ageMyFriend = 14
 const age = (myAge + ageMyFriend)
 alert(age)
+// 3
+const a = 234
+const b = 567
+const c = (a + b)
+console.log(c)
+// 4
+const a1 = 5 + 3
+console.log(a1)
+
+const a2 = 5 - 3
+console.log(a2)
+
+const a3 = 5 * 3
+console.log(a3)
+
+const a4 = 5 / 3
+console.log(a4)
+// let не приймає
+// 5
+const a5 = 5 % 3
+alert(a5)
+
+const a6 = 3 % 5
+alert(a6)
+
+const a7 = 5 + '3'
+alert(a7)
+
+const a8 = '5' + 3
+alert(a8)
+
+const a9 = 75 + 'цілих кілограмм'
+alert(a9)
+// 6
+const height = 23
+const width = 10
+const square = (height * width) + 'cm^2'
+console.log(square)
+// 7
+const heightC = 10
+const dc = 4**2
+const pi = 314 / 100
+const v = (pi*dc*heightC) + 'm^3'
+console.log(v)
+// 8
+const n = 3
+const m = 4
+const k = Math.sqrt(m**2 + n**2)
+console.log(k)
+// THE END
