a353c527cb36aa3c350771882112756f48a8aabd
artdobro <105646581+artdobro@users.noreply.github.com>
2022-11-29 17:32:19 +0200

    Hw

---
+++ index.js
@@ -0,0 +1,35 @@
+// first part
+// a
+const a = "Привіт"
+console.log(typeof a)
+
+const b = 123
+console.log(typeof b)
+
+const c = true
+console.log(typeof c)
+
+const d = "true"
+console.log(typeof d)
+// b
+const num = 9
+const txt = "Hello world maybe"
+const bool = true
+const none = null
+// not Sophie
+const name = undefined
+
+let numby = 10
+let sting = "Famous singer"
+let boolk = false 
+let nothing = null
+let nemoy;
+// second part
+// 1
+const userName = prompt("What you're name polzovatel:")
+console.log(userName)
+// 2
+const myAge = 14
+const ageMyFriend = 14
+const age = (myAge + ageMyFriend)
+alert(age)
